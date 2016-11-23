0221e6bf09f8c73e2e41d180368700f286f25563
Caner Kaynar <caner.kaynar@ciceksepeti.com>
2016-11-23 15:08:45 +0300

    almost done

---
+++ src/vendor.js
@@ -0,0 +1,8 @@
+/* This file contains references to the vendor libraries we're using in this project.
+This is used by webpack in the production build only. They can be cached until one of them changed.
+Any files that arent referenced here will be bundled into main.js for the production build.
+jquery,react,bootstrap... */
+
+/* eslint-disable no-console-vars */
+
+import fetch from 'whatwg-fetch';
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,6 @@
 import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -13,7 +15,15 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+
+    //Create HTML file that includes reference to bundled JS. Not need in index.html <script src="bundle.js"></script>
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
